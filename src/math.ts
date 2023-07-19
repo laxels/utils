6f55785d36a4bd0a22a6e559bb04bcdb1e2830c9
Leonardo Axel Setyanto <laxels@gmail.com>
2023-07-18 17:00:13 -0700

    isPrime

--- src/math.ts
+++ src/math.ts
@@ -13,3 +13,30 @@ export function multiply(a: number, b: number): number {
 export function divide(a: number, b: number): number {
   return a / b;
 }
+
+const isPrimeCache = new Map<number, boolean>();
+export function isPrime(x: number): boolean {
+  const cached = isPrimeCache.get(x)!;
+  if (cached != null) {
+    return cached;
+  }
+  const result = _isPrime(x);
+  isPrimeCache.set(x, result);
+  return result;
+}
+
+function _isPrime(x: number): boolean {
+  if (x <= 1 || x % 1 !== 0) {
+    return false;
+  }
+
+  let p = 2;
+  while (p <= Math.sqrt(x)) {
+    if (x % p === 0) {
+      return false;
+    }
+    p++;
+  }
+
+  return true;
+}
