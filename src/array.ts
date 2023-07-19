f4201d6e9fc644cb00192d5b7cfdb7fe1e6fc454
Leonardo Axel Setyanto <laxels@gmail.com>
2023-07-18 17:38:32 -0700

    find, findIndex

--- src/array.ts
+++ src/array.ts
@@ -33,12 +33,20 @@ export function map<T, U>(fn: (x: T) => U): (xs: T[]) => U[] {
   return (xs: T[]) => xs.map(fn);
 }
 
+export function reduce<T, U>(fn: (acc: U, x: T) => U, init: U): (xs: T[]) => U {
+  return (xs: T[]) => xs.reduce(fn, init);
+}
+
 export function filter<T>(fn: (x: T) => unknown): (xs: T[]) => T[] {
   return (xs: T[]) => xs.filter(fn);
 }
 
-export function reduce<T, U>(fn: (acc: U, x: T) => U, init: U): (xs: T[]) => U {
-  return (xs: T[]) => xs.reduce(fn, init);
+export function find<T>(fn: (x: T) => unknown): (xs: T[]) => Maybe<T> {
+  return (xs: T[]) => xs.find(fn);
+}
+
+export function findIndex<T>(fn: (x: T) => unknown): (xs: T[]) => number {
+  return (xs: T[]) => xs.findIndex(fn);
 }
 
 export function some<T>(fn: (x: T) => unknown): (xs: T[]) => boolean {
